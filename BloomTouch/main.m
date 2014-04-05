41e0a78adc5fe8e08b98fbdd3987d666345438e7
Nathan Burgers <nateburgers@gmail.com>
2014-04-05 14:35:26 -0400

    Initial Commit

---
+++ BloomTouch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BloomTouch
+//
+//  Created by Nathan Burgers on 4/5/14.
+//  Copyright (c) 2014 Nathan Burgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTAppDelegate class]));
+    }
+}
---
+++ BloomTouchTests/BloomTouchTests.m
@@ -0,0 +1,34 @@
+//
+//  BloomTouchTests.m
+//  BloomTouchTests
+//
+//  Created by Nathan Burgers on 4/5/14.
+//  Copyright (c) 2014 Nathan Burgers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BloomTouchTests : XCTestCase
+
+@end
+
+@implementation BloomTouchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
